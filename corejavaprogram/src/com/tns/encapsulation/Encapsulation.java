ade067afd40cb49019f14774680f05e39ff6b509
admin <admin@DESKTOP-RMF940T>
2024-06-14 17:50:11 +0530

    commit message

---
+++ corejavaprogram/src/com/tns/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.encapsulation;
+//demo encapsulation
+
+public class Encapsulation {
+	int serialnumber;//properities or data members
+	String name;
+	int age;
+	
+	public void show() {  //method or member function
+		System.out.println("serialnumber  "+serialnumber+"  "+"name   "+name+"  "+"age   "+age+"  ");
+		
+	}
+
+}
---
+++ corejavaprogram/src/com/tns/encapsulation/Exce.java
@@ -0,0 +1,16 @@
+package com.tns.encapsulation;
+
+public class Exce {
+	
+	public static void main(String[] args) {
+		Encapsulation e=new Encapsulation();
+		e.serialnumber=1;
+		e.name="rashmi";
+		e.age=24;
+		
+		e.show();
+		
+	}
+			
+
+}
